b27800c9f5748d9e78e6e5e61ebf3efd818bc6dd
VMCoud <li2654108@gmail.com>
2023-03-04 21:28:31 +0800

    1

--- backend/utils/db.js
+++
@@ -1,8 +0,0 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-
-const adapter = new FileSync(process.env.ZU_DATAPATH || "data/db.json");
-
-const db = low(adapter);
-
-module.exports = db;
