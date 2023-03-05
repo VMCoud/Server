6d651b8d6c4340b94d024c461988a927193c4cb3
VMCoud <li2654108@gmail.com>
2023-03-05 19:51:43 +0800

    update

--- planet.sh
+++ planet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/lib/zerotier-one
+cp -rf /Server/mkmoonworld-x86_64 /var/lib/zerotier-one
 chmod 777 mkmoonworld-x86_64
 zerotier-idtool initmoon identity.public > moon.json
 moonip="[\"$ZU_DEFAULT_IP_PROT\"]"
@@ -10,8 +11,8 @@ zerotier-idtool genmoon moon.json
 cp *.moon moons.d
 mv world.bin planet
 file="/app/frontend/build/static/$ZU_DEFAULT_USERNAME"
-if [ ! -f "$file" ]; then
-  mkdir $file
+if [ ! -d "$file" ]; then
+ mkdir $file
 fi
 cp -f /var/lib/zerotier-one/planet $file/planet
 echo Run Planet is generated successfully...
